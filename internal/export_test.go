ceb9cbd36d94939a46cf1d15e4cf835e6b7b41cf
Constantino Antunes <constantino.antunes@checkmarx.com>
2021-08-18 12:57:35 +0100

    AST-4423: refactor tests

--- internal/export_test.go
+++ internal/export_test.go
@@ -8,18 +8,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCreateExportFileName(t *testing.T) {
-	now := time.Date(2021, time.August, 18, 12, 27, 34, 0, time.UTC)
-	prefix := "test"
-
-	result := CreateExportFileName(prefix, now)
-
-	expected := "test-2021-08-18-12-27-34.zip"
-	assert.Equal(t, expected, result)
-}
+const (
+	prefix = "test"
+)
 
 func TestCreateExport(t *testing.T) {
-	prefix := "test"
 	export, err := CreateExport(prefix)
 	assert.NoError(t, err)
 
@@ -31,7 +24,6 @@ func TestCreateExport(t *testing.T) {
 }
 
 func TestExportClean(t *testing.T) {
-	prefix := "test"
 	export, err := CreateExport(prefix)
 	assert.NoError(t, err)
 
@@ -41,4 +33,13 @@ func TestExportClean(t *testing.T) {
 	_, statErr := os.Stat(export.TmpDir)
 	assert.Error(t, statErr)
 	assert.True(t, os.IsNotExist(statErr))
+}
+
+func TestCreateExportFileName(t *testing.T) {
+	now := time.Date(2021, time.August, 18, 12, 27, 34, 0, time.UTC)
+
+	result := CreateExportFileName(prefix, now)
+
+	expected := "test-2021-08-18-12-27-34.zip"
+	assert.Equal(t, expected, result)
 }
