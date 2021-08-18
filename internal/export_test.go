e921115b12d5cc84d97b3f67beb90f0a567912b5
Constantino Antunes <constantino.antunes@checkmarx.com>
2021-08-18 13:05:53 +0100

    AST-4423: test add file

--- internal/export_test.go
+++ internal/export_test.go
@@ -1,7 +1,9 @@
 package internal
 
 import (
+	"io/ioutil"
 	"os"
+	"path"
 	"testing"
 	"time"
 
@@ -21,6 +23,27 @@ func TestCreateExport(t *testing.T) {
 	assert.NoError(t, statErr)
 	assert.True(t, info.IsDir())
 	assert.Contains(t, export.TmpDir, prefix)
+}
+
+func TestExportAddFile(t *testing.T) {
+	export, err := CreateExport(prefix)
+	assert.NoError(t, err)
+
+	defer export.Clean()
+	addErr := export.AddFile("test1.txt", []byte("this is test1"))
+	assert.NoError(t, addErr)
+
+	expectedFileList := []string{"test1.txt"}
+	assert.Equal(t, expectedFileList, export.FileList)
+
+	test1FileName := path.Join(export.TmpDir, "test1.txt")
+	info, statErr := os.Stat(test1FileName)
+	assert.NoError(t, statErr)
+	assert.False(t, info.IsDir())
+
+	content, ioErr := ioutil.ReadFile(test1FileName)
+	assert.NoError(t, ioErr)
+	assert.Equal(t, "this is test1", string(content))
 }
 
 func TestExportClean(t *testing.T) {
