ff70a9134c69f81163722462be51aee20a5ca0dc
Constantino Antunes <constantino.antunes@checkmarx.com>
2021-12-21 16:23:12 +0000

    AST-7808: add query id repo

---
+++ internal/ast/models.go
@@ -0,0 +1,8 @@
+package ast
+
+type (
+	Query struct {
+		ID         uint64
+		SourcePath string
+	}
+)
--- internal/ast/query_id_repo.go
+++ internal/ast/query_id_repo.go
@@ -1,17 +1,42 @@
 package ast
 
-import "fmt"
+import (
+	_ "embed"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/pkg/errors"
+)
+
+//go:embed all_queries.json
+var AllQueries string
 
 type QueryIDProvider interface {
 	GetQueryID(language, name, group string) (string, error)
 }
 
-type QueryIDRepo struct{}
+type QueryIDRepo struct {
+	queries []Query
+}
 
-func NewQueryIDRepo() *QueryIDRepo {
-	return &QueryIDRepo{}
+func NewQueryIDRepo(allQueries string) (*QueryIDRepo, error) {
+	var queries []Query
+	unmarshalErr := json.Unmarshal([]byte(allQueries), &queries)
+	if unmarshalErr != nil {
+		return nil, errors.Wrap(unmarshalErr, "could not unmarshal queries json")
+	}
+	return &QueryIDRepo{
+		queries,
+	}, nil
 }
 
 func (e *QueryIDRepo) GetQueryID(language, name, group string) (string, error) {
-	return "", fmt.Errorf("not implemented")
+	sourcePath := fmt.Sprintf("queries/%s/%s/%s/%s.cs", language, group, name, name)
+	for _, query := range e.queries {
+		if query.SourcePath == sourcePath {
+			return strconv.FormatUint(query.ID, 10), nil
+		}
+	}
+	return "", fmt.Errorf("unknown source path: %s", sourcePath)
 }
