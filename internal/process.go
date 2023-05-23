d293a2f21005535ddffe591f134de4f031b335e5
Constantino Antunes <constantino.antunes@checkmarx.com>
2023-05-23 12:55:47 +0100

    Fix log message

--- internal/process.go
+++ internal/process.go
@@ -182,7 +182,7 @@ func RunExport(args *Args) error {
 		}
 	} else {
 		var exportCreateErr error
-		log.Info().Msg("exporting from folder")
+		log.Info().Msg("exporting from local manipulated folder")
 		exportValues, exportCreateErr = export2.CreateExportFromLocal(args.InputFolder, args.RunTime)
 
 		if exportCreateErr != nil {
