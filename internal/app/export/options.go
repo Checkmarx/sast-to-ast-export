8577d294d9fb2c196150f1e66a1d1a72ddbeb1bb
Joao Alves <joaor.alves@checkmarx.com>
2025-01-14 11:24:29 +0000

    rollback

--- internal/app/export/options.go
+++ internal/app/export/options.go
@@ -11,7 +11,7 @@ const (
 	ProjectsOption = "projects"
 	// QueriesOption represent custom queries
 	QueriesOption = "queries"
-	// PresetsOption Represent presets
+	// PresetsOption represent presets
 	PresetsOption = "presets"
 )
 
